c09235e20827ad0baaa934051e61bae3f3407d0f
101da <davidmueller85@gmail.com>
2016-10-03 11:53:50 +0200

    Adding content

---
+++ Allgemein.js
@@ -0,0 +1,12 @@
+/*var changeCSS = document.getElementById("table");
+
+changeCSS.onmouseover = function() {mouseOver()};
+changeCSS.onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    changeCSS.style.color = "red";
+}
+
+function mouseOut() {
+    changeCSS.style.color = "black";
+}*/
