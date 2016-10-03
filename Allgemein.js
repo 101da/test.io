84a298cba82df72ed3bc2f95c18ad2df5be8c810
101da <davidmueller85@gmail.com>
2016-10-03 22:01:07 +0200

    Added an Slideshow with Graphics
    To Do: give approbiate credits for the second picture

--- Allgemein.js
+++ Allgemein.js
@@ -1,12 +1,25 @@
-/*var changeCSS = document.getElementById("table");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-changeCSS.onmouseover = function() {mouseOver()};
-changeCSS.onmouseout = function() {mouseOut()};
-
-function mouseOver() {
-    changeCSS.style.color = "red";
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function mouseOut() {
-    changeCSS.style.color = "black";
-}*/
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display="none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display="block";
+  dots[slideIndex-1].className += " active";
+}
